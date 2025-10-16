95e71a4b132223efbb49487195c6b003652e721b
farukbahatekin <farukbahatekin@gmail.com>
2025-10-16 19:03:43 +0300

    variables

---
+++ 01_variables/01_variables/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string myFriendName;
+            string myFriendName2;
+
+            myFriendName = "ayhan";
+
+            Console.WriteLine(myFriendName);
+            Console.WriteLine("Senin Adin Ne?");
+
+            myFriendName2 = Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Merhaba {myFriendName2},{myFriendName} beyin paneline goz atiyorsunuz");
+
+        }
+    }
+}
